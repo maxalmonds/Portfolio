078a2bd3f359895cdd7056bb9d291395b47adeae
ssfund <rozahimki@gmail.com>
2026-04-18 19:35:55 +0300

    Fix Route Exact Path

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Switch, Route } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <Switch>
+        <Route exact path="/">
+          <App />
+        </Route>
+      </Switch>
   </React.StrictMode>,
   document.getElementById("root")
 );
